3c4772b67122fa6915aa76292f7515feaecaa975
somchit515 <113957644+somchit515@users.noreply.github.com>
2026-05-17 19:47:04 +0700

    fix: use DB_URI for all non-development environments (lowercase NODE_ENV)

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connectDatabase = () => {
   let DB_URI = "";
 
-  if (process.env.NODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_LOCAL_URI;
-  if (process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
+  if (process.env.NODE_ENV === "development") DB_URI = process.env.DB_LOCAL_URI;
+  else DB_URI = process.env.DB_URI;
 
   mongoose.connect(DB_URI)
     .then((con) => {
